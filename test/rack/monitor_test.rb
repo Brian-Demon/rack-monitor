9c86c302cd22fffbec573ef0ecfd28a538eeb34e
Brian-Demon <BrianWMatejka@gmail.com>
2021-05-24 15:53:00 -0500

    tests

--- test/rack/monitor_test.rb
+++ test/rack/monitor_test.rb
@@ -1,30 +1,31 @@
 require "test_helper"
+require "byebug"
 
 class Rack::MonitorTest < Minitest::Test
   include Rack::Test::Methods
 
   def exception_log
-    File.open("exceptions.log", "a+")
+    ::File.open("exceptions.log", "a+")
   end
 
   def exception_log_length
     file = exception_log
-    length = file.read.length
+    length = file.readlines.length
     file.close
     length
   end
 
   def app
-    builder = Rack::Builder.new
-    builder.use Rack::Monitor
-    builder.run lambda { |env| [200, {}, ["Welcome!"]]}
+    Rack::Builder.new do |builder|
+      builder.use Rack::Monitor, "exceptions.log"
+      builder.run lambda { |env| [200, {}, ["Welcome!"]]}
+    end.to_app
   end
 
   def test_ping_responds_pong
     get "/_ping"
-
     assert last_response.ok?
-    assert_equal last_response.body, "PONG"
+    assert_equal "PONG", last_response.body
   end
 
   def test_boom_responds_500
@@ -42,6 +43,13 @@ class Rack::MonitorTest < Minitest::Test
 
   def test_passes_through_requests
     get "/foo"
+    
+    assert last_response.ok?
+    assert_equal "Welcome!", last_response.body
+  end
+
+  def test_post_request_passes_through
+    post "/foo"
     
     assert last_response.ok?
     assert_equal "Welcome!", last_response.body
